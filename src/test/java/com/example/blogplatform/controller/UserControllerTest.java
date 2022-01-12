ece0cd5701517a843cfa52d3c4d7f0cd99ecd28b
nuelsax <aliunuel@gmail.com>
2022-01-12 15:04:44 +0100

    third Commit

---
+++ src/test/java/com/example/blogplatform/controller/UserControllerTest.java
@@ -0,0 +1,275 @@
+package com.example.blogplatform.controller;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
+
+import com.example.blogplatform.models.User;
+import com.example.blogplatform.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ContextConfiguration(classes = {UserController.class})
+@ExtendWith(SpringExtension.class)
+class UserControllerTest {
+    @Autowired
+    private UserController userController;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void testUpdateUser() throws Exception {
+        User user = new User();
+        user.setDateOfBirth("2020-03-01");
+        user.setEmail("jane.doe@example.org");
+        user.setFirstName("Jane");
+        user.setId(123L);
+        user.setLastName("Doe");
+        user.setPassword("iloveyou");
+        user.setPhoneNumber("4105551212");
+        when(this.userService.updateUser((User) any(), anyLong())).thenReturn(user);
+
+        User user1 = new User();
+        user1.setDateOfBirth("2020-03-01");
+        user1.setEmail("jane.doe@example.org");
+        user1.setFirstName("Jane");
+        user1.setId(123L);
+        user1.setLastName("Doe");
+        user1.setPassword("iloveyou");
+        user1.setPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(user1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/blog/users/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(
+                                "{\"id\":123,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane.doe@example.org\",\"phoneNumber\":\"4105551212"
+                                        + "\",\"dateOfBirth\":\"2020-03-01\",\"password\":\"iloveyou\"}"));
+    }
+
+    @Test
+    void testDeleteUser() throws Exception {
+        when(this.userService.deleteUser(anyLong())).thenReturn(new ResponseEntity<>(HttpStatus.CONTINUE));
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/blog/users/{id}", 123L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(100));
+    }
+
+    @Test
+    void testGetAllUsers() throws Exception {
+        when(this.userService.getAllUser()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/blog/users");
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testGetAllUsers2() throws Exception {
+        when(this.userService.getAllUser()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/blog/users");
+        getResult.characterEncoding("Encoding");
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testGetAllUsers3() throws Exception {
+        when(this.userService.getAllUser()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/blog/users");
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testGetAllUsers4() throws Exception {
+        when(this.userService.getAllUser()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/blog/users");
+        getResult.characterEncoding("Encoding");
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testGetUserById() throws Exception {
+        User user = new User();
+        user.setDateOfBirth("2020-03-01");
+        user.setEmail("jane.doe@example.org");
+        user.setFirstName("Jane");
+        user.setId(123L);
+        user.setLastName("Doe");
+        user.setPassword("iloveyou");
+        user.setPhoneNumber("4105551212");
+        when(this.userService.getUserbyId(anyLong())).thenReturn(user);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/blog/users/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(
+                                "{\"id\":123,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane.doe@example.org\",\"phoneNumber\":\"4105551212"
+                                        + "\",\"dateOfBirth\":\"2020-03-01\",\"password\":\"iloveyou\"}"));
+    }
+
+    @Test
+    void testLogin() throws Exception {
+        User user = new User();
+        user.setDateOfBirth("2020-03-01");
+        user.setEmail("jane.doe@example.org");
+        user.setFirstName("Jane");
+        user.setId(123L);
+        user.setLastName("Doe");
+        user.setPassword("iloveyou");
+        user.setPhoneNumber("4105551212");
+        when(this.userService.getUserbyId(anyLong())).thenReturn(user);
+
+        User user1 = new User();
+        user1.setDateOfBirth("2020-03-01");
+        user1.setEmail("jane.doe@example.org");
+        user1.setFirstName("Jane");
+        user1.setId(123L);
+        user1.setLastName("Doe");
+        user1.setPassword("iloveyou");
+        user1.setPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(user1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/blog/users/login/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("succesfully logged in"));
+    }
+
+    @Test
+    void testLogin2() throws Exception {
+        User user = new User();
+        user.setDateOfBirth("2020-03-01");
+        user.setEmail("?");
+        user.setFirstName("Jane");
+        user.setId(123L);
+        user.setLastName("Doe");
+        user.setPassword("iloveyou");
+        user.setPhoneNumber("4105551212");
+        when(this.userService.getUserbyId(anyLong())).thenReturn(user);
+
+        User user1 = new User();
+        user1.setDateOfBirth("2020-03-01");
+        user1.setEmail("jane.doe@example.org");
+        user1.setFirstName("Jane");
+        user1.setId(123L);
+        user1.setLastName("Doe");
+        user1.setPassword("iloveyou");
+        user1.setPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(user1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/blog/users/login/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("invalid credentials"));
+    }
+
+    @Test
+    void testLogin3() throws Exception {
+        User user = new User();
+        user.setDateOfBirth("2020-03-01");
+        user.setEmail("jane.doe@example.org");
+        user.setFirstName("Jane");
+        user.setId(123L);
+        user.setLastName("Doe");
+        user.setPassword("?");
+        user.setPhoneNumber("4105551212");
+        when(this.userService.getUserbyId(anyLong())).thenReturn(user);
+
+        User user1 = new User();
+        user1.setDateOfBirth("2020-03-01");
+        user1.setEmail("jane.doe@example.org");
+        user1.setFirstName("Jane");
+        user1.setId(123L);
+        user1.setLastName("Doe");
+        user1.setPassword("iloveyou");
+        user1.setPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(user1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/blog/users/login/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("invalid credentials"));
+    }
+
+    @Test
+    void testRegisterUser() throws Exception {
+        when(this.userService.getAllUser()).thenReturn(new ArrayList<>());
+
+        User user = new User();
+        user.setDateOfBirth("2020-03-01");
+        user.setEmail("jane.doe@example.org");
+        user.setFirstName("Jane");
+        user.setId(123L);
+        user.setLastName("Doe");
+        user.setPassword("iloveyou");
+        user.setPhoneNumber("4105551212");
+        String content = (new ObjectMapper()).writeValueAsString(user);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/blog/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+}
+
